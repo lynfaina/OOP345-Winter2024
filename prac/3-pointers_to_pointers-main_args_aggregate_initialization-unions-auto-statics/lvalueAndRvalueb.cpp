97c057626fab86b8ebbb92867af48fe454e3c3c2
lynfaina <gelinefaina@gmail.com>
2024-01-02 10:48:00 -0500

    modified files for testing

--- prac/3-pointers_to_pointers-main_args_aggregate_initialization-unions-auto-statics/lvalueAndRvalueb.cpp
+++ prac/3-pointers_to_pointers-main_args_aggregate_initialization-unions-auto-statics/lvalueAndRvalueb.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 void display(int& a) {
@@ -12,5 +13,14 @@ int main() {
     int i = 10;
     display(i); // left value 10
     display(123); // right value 123
+
+    // added tests
+    display(i = 20); // left value 20
+    display(16789); // right value 16789
+    display(i); // left value 20
+    display((int)12.05); // right value 12
+    display(i = (int)15.67); // left value 15
+    display(static_cast<int>(std::ceil(15.67))); // rightvalue 16 // rounded up
+
     return 0;
 }
--- prac/3-pointers_to_pointers-main_args_aggregate_initialization-unions-auto-statics/namelessStructure.cpp
+++ prac/3-pointers_to_pointers-main_args_aggregate_initialization-unions-auto-statics/namelessStructure.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring> // added test
 using namespace std;
 
 class Student {
@@ -19,16 +20,33 @@ class Student {
     } */ // this is the type
     // m_name is the instance
 public:
+    Student(string first, string last):m_name{first, last} {};
+
     // to copy into another structure of the same type
-    void foo {
+    void foo(const char* name) { // const char* name added test
         auto n = m_name;
         // n is a local variable
         // auto takes care of the type
         // and sets to m_name
         // auto is for things that don't have name or type
     }
-}
+
+    // added test
+    string getFirstName() const {
+        return m_name.first;
+    }
+
+    string getLastName() const {
+        return m_name.last;
+    }
+};
 
 int main () {
+    // added tests
+    Student s("nicole", "garcia");
+    s.foo("nicole");
+
+    cout << s.getFirstName() << " " << s.getLastName() << endl;
+
     return 0;
 }
