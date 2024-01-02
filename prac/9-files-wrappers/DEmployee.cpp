80aa041e4fb8cd5ca83a9cd481d6672904ecf0c1
lynfaina <gelinefaina@gmail.com>
2024-01-01 22:17:34 -0500

    added files in files-wrappers repository

---
+++ prac/9-files-wrappers/DEmployee.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include "DEmployee.h"
+using namespace std;
+void Employee::strcpy(char* des, const char* src) {
+    while (src && des && *src) *des++ = *src++;
+    if(des)*des = 0;
+}
+size_t Employee::strlen(const char* s) {
+    size_t i = 0;
+    while (s && s[i])i++;
+    return i;
+}
+Employee::Employee(double sal, int emp, const char* nm) {
+    salary = sal;
+    empno = emp;
+    name = new char[strlen(nm) + 1];
+    strcpy(name, nm);
+}
+Employee::Employee(const Employee& E) {
+    operator=(E);
+}
+Employee::Employee(Employee&& E) {
+    operator=(move(E));
+}
+Employee& Employee::operator=(const Employee& E) {
+    if (this != &E) {
+        delete[] name;
+        name = new char[strlen(E.name) + 1];
+        strcpy(name, E.name);
+        salary = E.salary;
+        empno = E.empno;
+    }
+    return *this;
+}
+Employee& Employee::operator=(Employee&& E) {
+    if (this != &E) {
+        delete[] name;
+        name = E.name;
+        E.name = nullptr;
+        salary = E.salary;
+        empno = E.empno;
+    }
+    return *this;
+}
+Employee::~Employee() {
+    delete[] name;
+}
+bool Employee::operator<(const Employee& rOper) const {
+    size_t i = 0;
+    while (name && rOper.name && name[i] && rOper.name[i] && name[i] == rOper.name[i]);
+    return name && rOper.name && name[i] < rOper.name[i];
+}
+ostream& Employee::print(ostream& os)const {
+    return os << name << "," << empno << "," << fixed << setprecision(2) << salary;
+}
+bool Employee::write(std::ofstream& ofs, ofstream& nfs) const { // writing one employee 
+    std::streampos nameLoc = nfs.tellp();
+    // nameLoc will be the address of the name inside the file
+    // and then tell me what is the length of the name
+    // because each one has different length, we need to know
+    // how many we have to read after 
+    size_t nameLen = strlen(name);
+    nfs.write(name, nameLen); // write in nfs in the name file system,
+    // Employee in file = *this (shallow assignment) // does exactly the same
+    // shallow copy - copies one-by-one regardless of what is the data
+        // if you put garbage, you get garbage
+    // while deep copy is to look at the data, interpret it and pass it along
+    // write the name up to name lenghts
+    ofs.write((const char*)this, sizeof(*this)); // writing in the object // first we're writing the obejct itself
+    // go to the beginning of the object
+    // cast it into a constant character pointer
+    // write this object into memory to size of this
+        // which sizes are a double = 8 bytes
+        // int = 8 bytes (64-bit)
+        // char* = 8 bytes
+        // 24 bytes total
+    ofs.write((const char*)&nameLoc, sizeof(nameLoc)); // go to the address of name location and write it in a file
+    ofs.write((const char*)&nameLen, sizeof(nameLen));
+    // the sizes are always identical
+    return ofs && nfs;
+    // the data that we have from the class is now saved in two files
+}
+bool Employee::read(ifstream& ifs, ifstream& nfs) {
+    // its gonna read the salary, the employee number, and some garbage address
+    std::streampos nameLoc{};
+    size_t nameLen{};
+    delete[] name;
+    // *this = Employee in file (shallow assignment) 
+        // go to the address of the current object byte-by-byte, write it into the file
+        // it looks like the first byte is 1/8 of that double
+        // doesn't care its a double
+        // it goes through the thing that 24 bytes one-by-one and writes it into the file
+        // it doesn't know its employee, it just knows its an address in memory and this is its size 
+        // shallow copy when you do not implement the copy constructor amd copy assignment
+    ifs.read((char*)this, sizeof(*this));
+    ifs.read((char*)&nameLoc, sizeof(nameLoc)); // read the name location from the file
+    ifs.read((char*)&nameLen, sizeof(nameLen)); // read the name length from the file
+    if (ifs) { // if the file was successfully opened
+        name = new char[nameLen + 1]; // allocate enough space for the name // overwrite the name
+        nfs.seekg(nameLoc);  // seek to the name location
+        nfs.read(name, nameLen); // read into the name up to the name length character
+        name[nameLen] = 0; // set the last character to 0
+    } else {
+        name = nullptr;
+    }
+    return ifs && nfs;
+}
+std::ostream& operator<<(std::ostream& os, const Employee& E) {
+    return E.print(os);
+}
+
+
+
+// index file is a quicker way to search
---
+++ prac/9-files-wrappers/DEmployee.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+class Employee {
+    double salary{};
+    int empno{};
+    char* name{}; // the name is not inside the class anymore compared to the previous Employee-copy.h file
+    //  char name[52]; // this will end up with the garbage
+    // because its gonna save the empno, the salary, and the address of the name
+    // at the moment the program is running in the memory
+    // but for this, for reading and writing, we don't only enter an object file system
+    // but also the name
+    // and we'll save the object that is all the same size in 
+    // a binary file with exact record size then and put the name sequential in a file
+    // but as we're writing the name, we'll gonna ask the C, tell me where the file, 
+    // tellp to tell where the information is being written
+    // so its gonna tell that this name is being written as address 952
+    // we'll take that 952 and save it 
+public:
+    static void strcpy(char* des, const char* src);
+    static size_t strlen(const char* s);
+    Employee(double sal = 0.0, int emp = 0, const char* nm = "");
+    Employee(const Employee&);
+    Employee(Employee&&);
+    Employee& operator=(const Employee&);
+    Employee& operator=(Employee&&);
+    ~Employee();
+    bool operator<(const Employee& rOper)const;
+    std::ostream& print(std::ostream& os)const;
+    bool write(std::ofstream& ofs, std::ofstream& nfs)const;
+    bool read(std::ifstream& ifs, std::ifstream& nfs);
+};
+std::ostream& operator<<(std::ostream& os, const Employee& E);
